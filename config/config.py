3b4af65c916775b7bd276721e732c110cc15fe6f
Arunhald <87762748+Arunhald@users.noreply.github.com>
2022-08-14 19:00:24 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -68,7 +68,7 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", https://t.me/smart_aruney_bots
 )  # Example:- https://t.me/TheYukki
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", None
+    "SUPPORT_GROUP", https://t.me/Tessa_Queen_supports
 )  # Example:- https://t.me/YukkiSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
